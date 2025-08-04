0882f2b64a7aa977cd3433fd606276ebef9464ad
seok <rudtjr1206@naver.com>
2025-08-04 11:49:28 +0900

    feat - onboarding 회원가입 여부 체크 기능 추가

--- composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/onboarding/OnboardingRoute.kt
+++ composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/onboarding/OnboardingRoute.kt
@@ -1,17 +1,14 @@
 package com.pinup.pinup.ui.onboarding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import com.pinup.pinup.platform.ContextFactory
 import kotlinx.coroutines.flow.collectLatest
 import org.koin.compose.viewmodel.koinViewModel
-import org.koin.core.parameter.parametersOf
 
 @Composable
 fun OnboardingRoute(
-    contextFactory: ContextFactory,
     onMoveSignUpOnboarding: () -> Unit,
     onMoveLogin: () -> Unit,
-    viewModel: OnboardingViewModel = koinViewModel(parameters = { parametersOf(contextFactory) })
+    viewModel: OnboardingViewModel = koinViewModel()
 ) {
     LaunchedEffect(Unit) {
         viewModel.uiEvent.collectLatest {
@@ -23,6 +20,6 @@ fun OnboardingRoute(
     }
     OnboardingScreen(
         //TODO 임시, 추후 회원가입 여부 로직 추가
-        onClickStart = onMoveSignUpOnboarding
+        onClickStart = viewModel::onClickStart
     )
 }
