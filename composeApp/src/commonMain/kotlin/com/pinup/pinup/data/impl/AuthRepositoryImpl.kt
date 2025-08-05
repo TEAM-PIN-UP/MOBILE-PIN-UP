b99080fd97b49e436b864c2f9cd5cf106bc8add2
seok <rudtjr1206@naver.com>
2025-08-05 10:05:22 +0900

    feat - 자체 이메일 로그인 UseCase 추가

--- composeApp/src/commonMain/kotlin/com/pinup/pinup/data/impl/AuthRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/pinup/pinup/data/impl/AuthRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.pinup.pinup.data.impl
 
 import com.pinup.pinup.data.local.MembersLocalDataSource
 import com.pinup.pinup.data.remote.AuthRemoteDataSource
+import com.pinup.pinup.data.request.EmailLoginRequest
 import com.pinup.pinup.data.request.LoginRequest
 import com.pinup.pinup.data.request.SignUpRequest
 import com.pinup.pinup.data.response.LoginResponse
@@ -14,8 +15,12 @@ class AuthRepositoryImpl (
     private val authRemoteDataSource: AuthRemoteDataSource,
     private val membersLocalDataSource: MembersLocalDataSource,
 ): AuthRepository {
+    override suspend fun emailLogin(request: EmailLoginRequest): PResult<LoginResponse> {
+        return authRemoteDataSource.emailLogin(request)
+    }
+
     override suspend fun socialLogin(provider : String, socialId: String): PResult<LoginResponse> {
-        return authRemoteDataSource.login(
+        return authRemoteDataSource.socialLogin(
             request = LoginRequest(
                 provider = provider,
                 socialId = socialId
--- composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/login/LoginViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/login/LoginViewModel.kt
@@ -1,28 +1,25 @@
 package com.pinup.pinup.ui.login
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.pinup.pinup.domain.model.PResult
+import com.pinup.pinup.domain.usecase.EmailLoginUseCase
 import com.pinup.pinup.domain.usecase.SocialLoginUseCase
 import com.pinup.pinup.platform.ContextFactory
 import com.pinup.pinup.platform.hLog
+import com.pinup.pinup.ui.base.BaseViewModel
+import com.pinup.pinup.ui.base.UiEvent
+import com.pinup.pinup.ui.base.UiState
 import com.pinup.pinup.ui.login.model.SNSType
 import com.pinup.pinup.ui.login.model.SNSUserInfo
 import com.pinup.pinup.ui.login.sns.SNSLoginFactory
 import com.pinup.pinup.ui.login.sns.SNSLoginResultListener
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 class LoginViewModel (
     private val contextFactory: ContextFactory,
+    private val loginUseCase: EmailLoginUseCase,
     private val socialLoginUseCase: SocialLoginUseCase,
     private val snsLoginFactory: SNSLoginFactory
-) : ViewModel() {
-    private val _uiEvent = MutableSharedFlow<LoginUiEvent>()
-    val uiEvent: SharedFlow<LoginUiEvent>
-        get() = _uiEvent.asSharedFlow()
+) : BaseViewModel<LoginUiState, LoginUiEvent>(LoginUiState()) {
     private val loginResultListener = object : SNSLoginResultListener {
         override fun onCancel() {
             hLog("login cancel")
@@ -41,23 +38,38 @@ class LoginViewModel (
         snsLoginFactory.doLogin(snsType, contextFactory, loginResultListener)
     }
 
-    private fun login(snsLoginInfo: SNSUserInfo) = viewModelScope.launch {
-        when (val result = socialLoginUseCase.invoke(snsLoginInfo)) {
-            is PResult.Fail -> {
-                hLog("fail >> ${result.failState}")
-                if (result.failState.code == "E_MEMBER001") {
-                    _uiEvent.emit(LoginUiEvent.MoveSignUp(snsLoginInfo))
-                }
-            }
-            is PResult.Success -> {
-                hLog("success")
-                _uiEvent.emit(LoginUiEvent.MoveMain)
-            }
+    private fun login(snsLoginInfo: SNSUserInfo) {
+        viewModelScope.launch {
+            resultResponse(loginUseCase(snsLoginInfo), { emitEvent( LoginUiEvent.MoveMain ) }, { handleFailLogin(it, snsLoginInfo)} )
         }
+    }
+
+    private fun handleFailLogin(code : String, snsLoginInfo : SNSUserInfo) {
+//        if (code == StatusCode.Login.NOT_EXIST_MEMBER) {
+//            emitEvent(LoginUiEvent.MoveSignUp(snsLoginInfo))
+//        }
+    }
+
+    fun onIdChange(id: String) {
+        updateState { copy(id = id) }
+    }
+
+    fun onPasswordChange(password: String) {
+        updateState { copy(password = password) }
+    }
+
+    fun onClickLogin(){
+        //TODO: 로그인 로직 구현
     }
 }
 
-sealed interface LoginUiEvent {
+data class LoginUiState(
+    val id: String = "",
+    val password: String = "",
+    val isError : Boolean = false,
+) : UiState
+
+sealed interface LoginUiEvent : UiEvent {
     data class MoveSignUp(val snsLoginInfo: SNSUserInfo) : LoginUiEvent
     data object MoveMain : LoginUiEvent
 }
