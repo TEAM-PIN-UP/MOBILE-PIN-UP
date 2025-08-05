4caf6d399b78d51c2c98e3757a1485cdd30e9a04
seok <rudtjr1206@naver.com>
2025-08-05 09:12:59 +0900

    chore - 코드 리뷰 코멘트 반영

--- composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/theme/Texts.kt
+++ composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/theme/Texts.kt
@@ -6,14 +6,19 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 
 object Texts {
-    val ONBOARDING_TEXT = buildAnnotatedString {
-        withStyle(style = SpanStyle(color = Colors.White)) {
-            append("우리의 로컬 아카이브, ")
-        }
-        withStyle(style = SpanStyle(color = Colors.Main)) {
-            append("핀업")
+
+    object Onboarding{
+        val ONBOARDING_TEXT = buildAnnotatedString {
+            withStyle(style = SpanStyle(color = Colors.White)) {
+                append("우리의 로컬 아카이브, ")
+            }
+            withStyle(style = SpanStyle(color = Colors.Main)) {
+                append("핀업")
+            }
         }
     }
 
-    val WORD_START = "시작하기"
+    object Word {
+        const val WORD_START = "시작하기"
+    }
 }
