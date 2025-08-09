53a8b5fba4c5b4c654812f724f1d235a21f8b8cb
seok <rudtjr1206@naver.com>
2025-08-10 08:31:38 +0900

    feat - 이메일 인증 api 추가

---
+++ composeApp/src/commonMain/kotlin/com/pinup/pinup/data/request/EmailVerifyRequest.kt
@@ -0,0 +1,14 @@
+package com.pinup.pinup.data.request
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EmailVerifyRequest(
+    val email: String,
+    val code: String
+)
+
+@Serializable
+data class SendVerifyCodeRequest(
+    val code: String
+)
---
+++ composeApp/src/commonMain/kotlin/com/pinup/pinup/remote/api/EmailApi.kt
@@ -0,0 +1,16 @@
+package com.pinup.pinup.remote.api
+
+import com.pinup.pinup.data.request.EmailVerifyRequest
+import com.pinup.pinup.data.request.SendVerifyCodeRequest
+import com.pinup.pinup.data.response.PResponse
+import com.pinup.pinup.domain.model.PResult
+import de.jensklingenberg.ktorfit.http.Body
+import de.jensklingenberg.ktorfit.http.POST
+
+interface EmailApi {
+    @POST(MailPath.VERIFY_CODE)
+    suspend fun verifyEmail(@Body request: EmailVerifyRequest): PResult<PResponse<Unit>>
+
+    @POST(MailPath.SEND_CODE)
+    suspend fun sendVerifyCode(@Body request: SendVerifyCodeRequest): PResult<PResponse<Unit>>
+}
