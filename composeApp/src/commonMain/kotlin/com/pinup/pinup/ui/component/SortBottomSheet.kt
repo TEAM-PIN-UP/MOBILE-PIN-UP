6889fc3d57f23c4c1d093119a8d5b3906e8af6e8
seok <rudtjr1206@naver.com>
2025-08-11 11:18:00 +0900

    design - 정렬 bottomSheet 디자인 수정

--- composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/component/SortBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/component/SortBottomSheet.kt
@@ -1,18 +1,27 @@
 package com.pinup.pinup.ui.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.pinup.pinup.domain.model.SortType
 import com.pinup.pinup.extentions.clickableWithNoRipple
 import com.pinup.pinup.ui.theme.Colors
 import com.pinup.pinup.ui.theme.Typography
+import org.jetbrains.compose.resources.painterResource
+import pinup.composeapp.generated.resources.Res
+import pinup.composeapp.generated.resources.ic_check_main
 
 @Composable
 fun SortBottomSheet(
@@ -20,21 +29,39 @@ fun SortBottomSheet(
     allPermissionsGranted: Boolean,
     onSortTypeSelect: (SortType) -> Unit,
 ) {
-    Spacer(Modifier.height(24.dp))
-    SortType.entries.forEach {
-        if (allPermissionsGranted.not() && it == SortType.NEAR) return@forEach
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 10.dp)
-                .clickableWithNoRipple {
-                    onSortTypeSelect(it)
-                },
-            text = it.text,
-            color = if (it == selectedSortType) Colors.Error else Colors.Neutral800,
-            textAlign = TextAlign.Center,
-            style = Typography.H3
-        )
+    Spacer(Modifier.height(40.dp))
+    Column {
+        SortType.entries.forEach {
+            if (allPermissionsGranted.not() && it == SortType.NEAR) return@forEach
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .clickableWithNoRipple {
+                        onSortTypeSelect(it)
+                    },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (selectedSortType == it) {
+                    Image(
+                        painter = painterResource(Res.drawable.ic_check_main),
+                        contentDescription = null
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+
+                Text(
+                    text = it.text,
+                    color = Colors.Gray800,
+                    style = Typography.T2.copy(
+                        fontWeight = FontWeight.Medium
+                    )
+                )
+            }
+            Spacer(Modifier.height(30.dp))
+        }
     }
-    Spacer(Modifier.height(24.dp))
+
+    Spacer(Modifier.height(34.dp))
 }
