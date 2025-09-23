a7864199d19ce1bcd4e9af93880b68f5f11cffd7
hunihun <box6687@naver.com>
2025-09-23 21:43:25 +0900

    [DP-56] - 내용 작성란 줄바꿈 기능 추가

--- composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/reviewwrite/compose/WriteReviewScreen.kt
+++ composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/reviewwrite/compose/WriteReviewScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -29,6 +30,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -362,7 +365,11 @@ fun WriteReviewScreen(
                 ),
                 placeholder = Texts.PinLog.PINLOG_HINT,
                 onValueChange = onValueChange,
-                maxLength = maxLength
+                maxLength = maxLength,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Default,
+                    keyboardType = KeyboardType.Text,
+                )
             )
 
             if (reviewText.length < minLength || reviewText.length > maxLength) {
