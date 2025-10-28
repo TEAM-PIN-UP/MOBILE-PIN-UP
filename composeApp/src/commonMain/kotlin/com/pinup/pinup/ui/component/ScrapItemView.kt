c4464a003b229fc4272f27c62cbaeebe710c684e
정상훈 <sanghun.jeong@kurlycorp.com>
2025-10-28 16:40:12 +0900

    feat - [DP-97] 스크랩 썸네일 노출

--- composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/component/ScrapItemView.kt
+++ composeApp/src/commonMain/kotlin/com/pinup/pinup/ui/component/ScrapItemView.kt
@@ -1,5 +1,7 @@
 package com.pinup.pinup.ui.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -7,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
@@ -14,9 +17,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.pinup.pinup.domain.model.BookmarkedPlace
-import com.pinup.pinup.domain.model.ReviewedPlace
 import com.pinup.pinup.ui.theme.Colors
 import com.pinup.pinup.ui.theme.Typography
+import org.jetbrains.compose.resources.painterResource
+import pinup.composeapp.generated.resources.Res
+import pinup.composeapp.generated.resources.ic_empty_scrap
 
 @Composable
 fun ScrapItemView(
@@ -26,18 +31,35 @@ fun ScrapItemView(
     Column(
         modifier = modifier,
     ) {
-        RoundedBox(
-            modifier = Modifier
-                .fillMaxWidth(),
-        ) {
-            AsyncImage(
+        if (place.placeFirstReviewImageUrl.isEmpty()) {
+            RoundedBox(
                 modifier = Modifier
                     .fillMaxWidth()
                     .aspectRatio(105f / 90f),
-                model = place.placeFirstReviewImageUrl,
-                contentScale = ContentScale.Crop,
-                contentDescription = null
-            )
+                backgroundColor = Colors.Gray50
+            ) {
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Center),
+                    painter = painterResource( Res.drawable.ic_empty_scrap),
+                    contentDescription = null
+                )
+            }
+        } else {
+            RoundedBox(
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                AsyncImage(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(105f / 90f),
+                    model = place.placeFirstReviewImageUrl,
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null
+                )
+            }
         }
 
         Spacer(modifier = Modifier.height(8.dp))
