3c8ec457bd27ebddcd0494b04817a6d3c9f9ea53
seok <rudtjr1206@naver.com>
2025-11-07 21:32:35 +0900

    feat - [DP-49] 패키지명 변경

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -126,7 +126,7 @@ android {
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.pinup.pinup"
+        applicationId = "com.pinup.placePinup"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 3
