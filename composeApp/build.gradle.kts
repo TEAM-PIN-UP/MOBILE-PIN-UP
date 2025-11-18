3c006bae90869085de07555a7debd8e57b9eab8c
seok <rudtjr1206@naver.com>
2025-11-18 11:10:50 +0900

    feat - [DP-49] 버전업

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -129,8 +129,8 @@ android {
         applicationId = "com.pinup.placePinup"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "1.0.0"
+        versionCode = 7
+        versionName = "1.0.1"
         multiDexEnabled = true
 
         buildConfigField(
