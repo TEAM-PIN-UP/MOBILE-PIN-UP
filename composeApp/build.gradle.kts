0672f31e3046268327aa6668c5b513d5d8dd2cd7
seok <rudtjr1206@naver.com>
2025-11-30 19:40:38 +0900

    version up

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -129,7 +129,7 @@ android {
         applicationId = "com.pinup.placePinup"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0.3"
         multiDexEnabled = true
 
